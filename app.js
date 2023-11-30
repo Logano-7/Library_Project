e6fd7041576e192b4f1bdf85f5f5d83b6e40b3e7
Logan Martin <88mlogan@gmail.com>
2023-11-29 19:29:44 -0500

    Main Objective Functioning, some problems

--- app.js
+++ app.js
@@ -1,4 +1,79 @@
-console.log("Hello World!\n==========\n");
+class Book {
+  constructor(title, author, read) {
+    this.title = title;
+    this.author = author;
+    this.read = read;
+  }
+}
 
-// PROJECT Section
-console.log("PROJECT:\n==========\n");
+class Library {
+  constructor() {
+    this.bookCount = 0;
+    this.books = [];
+  }
+
+  markRead(id, checkbox) {
+    this.books.forEach((book) => {
+      if (book.id === id) {
+        book.read = true;
+        checkbox.checked = true;
+        checkbox.disabled = true;
+      }
+    });
+  }
+
+  addBook(title, author, read) {
+    const newBook = new Book(title, author, read);
+    newBook.id = this.bookCount++;
+
+    const tableBody = document.querySelector("tbody");
+    const newRow = document.createElement("tr");
+
+    const titleCell = document.createElement("td");
+    titleCell.textContent = newBook.title;
+
+    const authorCell = document.createElement("td");
+    authorCell.textContent = newBook.author;
+
+    const readCell = document.createElement("td");
+    const readCheckbox = document.createElement("input");
+    readCheckbox.type = "checkbox";
+    readCheckbox.checked = newBook.read;
+    readCheckbox.classList.add("checkBoxes");
+    if (newBook.read) {
+      readCheckbox.disabled = true;
+    }
+    readCell.appendChild(readCheckbox);
+
+    newRow.appendChild(titleCell);
+    newRow.appendChild(authorCell);
+    newRow.appendChild(readCell);
+
+    tableBody.appendChild(newRow);
+
+    this.books.push(newBook);
+
+    document.getElementById("title").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("read").checked = false;
+  }
+}
+
+const addButton = document.getElementById("addBookButton");
+addButton.addEventListener("click", () => {
+  const titleInput = document.getElementById("title").value;
+  const authorInput = document.getElementById("author").value;
+  const readCheckbox = document.getElementById("read").checked;
+
+  myLibrary.addBook(titleInput, authorInput, readCheckbox);
+});
+const myLibrary = new Library();
+
+const checkBoxes = document.querySelectorAll(".checkBoxes");
+checkBoxes.forEach((checkbox) => {
+  checkbox.addEventListener("click", () => {
+    if (checkbox === true) {
+      checkbox.disabled = true;
+    }
+  });
+});
