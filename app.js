a6a75e7c104ed2087f74cb4cf1785793010298b2
Logan Martin <88mlogan@gmail.com>
2023-12-02 19:35:32 -0500

    Minimized Code and Added Notes

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Create a class for the book info
 class Book {
   constructor(title, author, read) {
     this.title = title;
@@ -6,12 +7,14 @@ class Book {
   }
 }
 
+//Library class to store the books
 class Library {
   constructor() {
     this.bookCount = 0;
     this.books = [];
   }
 
+  // Mark the book as read
   markRead(id, checkbox) {
     this.books.forEach((book) => {
       if (book.id === id) {
@@ -22,10 +25,12 @@ class Library {
     });
   }
 
+  // Add the book to the library
   addBook(title, author, read) {
     const newBook = new Book(title, author, read);
     newBook.id = this.bookCount++;
 
+    // Create the table row and cells
     const tableBody = document.querySelector("tbody");
     const newRow = document.createElement("tr");
 
@@ -45,6 +50,7 @@ class Library {
     readCheckbox.checked = newBook.read;
     readCheckbox.classList.add("checkBoxes");
 
+    // Disable the checkbox if the book is already read
     if (newBook.read) {
       readCheckbox.disabled = true;
     }
@@ -52,29 +58,34 @@ class Library {
       this.markRead(newBook.id, readCheckbox);
     });
 
+    // Create the remove button
     removeButton.textContent = "X";
     removeButton.classList.add("removeBtn");
 
+    // Remove the book from the library
     removeButton.addEventListener("click", () => {
       const row = removeButton.parentNode.parentNode;
       row.parentNode.removeChild(row);
       this.removeBook(newBook.id);
     });
 
+    // Append the cells to the row
     readCell.append(readCheckbox, removeButton);
     newRow.appendChild(titleCell);
     newRow.appendChild(authorCell);
     newRow.appendChild(readCell);
-
     tableBody.appendChild(newRow);
 
+    // Push the book to the library
     this.books.push(newBook);
 
+    // Clear the input fields
     document.getElementById("title").value = "";
     document.getElementById("author").value = "";
     document.getElementById("read").checked = false;
   }
 
+  // Remove the book from the library
   removeBook(id) {
     this.books.forEach((book, index) => {
       if (book.id === id) {
@@ -83,8 +94,11 @@ class Library {
     });
   }
 }
+
+// Create MyLibrary object
 const myLibrary = new Library();
 
+// Add the book to the library
 const addButton = document.getElementById("addBookButton");
 addButton.addEventListener("click", () => {
   const titleInput = document.getElementById("title").value;
@@ -92,16 +106,4 @@ addButton.addEventListener("click", () => {
   const readCheckbox = document.getElementById("read").checked;
 
   myLibrary.addBook(titleInput, authorInput, readCheckbox);
-});
-
-const removeButton = document.getElementById("removeBookButton");
-removeButton.addEventListener("click", () => {
-  const checkBoxes = document.querySelectorAll(".checkBoxes");
-  checkBoxes.forEach((checkbox) => {
-    if (checkbox.checked) {
-      const row = checkbox.parentNode.parentNode;
-      row.parentNode.removeChild(row);
-      myLibrary.removeBook(checkbox.id);
-    }
-  });
 });
